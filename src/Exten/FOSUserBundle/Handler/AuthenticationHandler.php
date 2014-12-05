512fa9b1cb66947053832d16fce938d4bc13499a
seqq86@gmail.com <p.nikolajevs@accenture.com>
2014-12-05 16:49:55 +0200

    Ajax Auth

--- src/Exten/FOSUserBundle/Handler/AuthenticationHandler.php
+++ src/Exten/FOSUserBundle/Handler/AuthenticationHandler.php
@@ -2,53 +2,57 @@
 
 namespace Exten\FOSUserBundle\Handler;
 
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Router;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-
 
 class AuthenticationHandler
 implements AuthenticationSuccessHandlerInterface,
            AuthenticationFailureHandlerInterface
 {
+   
+    /**
+     * This is called when an interactive authentication attempt succeeds. This
+     * is called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @see \Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener
+     * @param Request        $request
+     * @param TokenInterface $token
+     * @return Response the response to return
+     */
     
-    protected $router;
-    protected $security;
-    protected $userManager;
-    protected $service_container;
-    
-    public function __construct(RouterInterface $router,SecurityContext $security, $userManager, $service_container)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
-        $this->router = $router;
-        $this->security = $security;
-        $this->userManager = $userManager;
-        $this->service_container = $service_container;
-        
-    }
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token) {
         if ($request->isXmlHttpRequest()) {
             $result = array('success' => true);
             $response = new Response(json_encode($result));
             $response->headers->set('Content-Type', 'application/json');
             return $response;
         }
-        return new RedirectResponse($this->router->generate('anag_new')); 
-    } 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception) {
-        
+    }
+
+    /**
+     * This is called when an interactive authentication attempt fails. This is
+     * called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @param Request                 $request
+     * @param AuthenticationException $exception    
+     * @return Response the response to return
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
         if ($request->isXmlHttpRequest()) {
             $result = array('success' => false, 'message' => $exception->getMessage());
             $response = new Response(json_encode($result));
             $response->headers->set('Content-Type', 'application/json');
             return $response;
         }
-        return new Response();
     }
 }
