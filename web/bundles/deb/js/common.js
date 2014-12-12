bf494e0e2ace94e0b509d852610dfe945698ebb0
seqq86@gmail.com <p.nikolajevs@accenture.com>
2014-12-12 17:11:27 +0200

    Small design addition to Authoriation

--- web/bundles/deb/js/common.js
+++ web/bundles/deb/js/common.js
@@ -1,4 +1,34 @@
 $(function () {
+    
+    $("#AuthForm").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Reset": function() {
+                $("#AuthForm").find('#email').val("");
+                $('#password').val("");
+            },
+            "Submit": function() {
+
+                var check_result = UserModule.CheckAuthFields(password_length);
+
+                if(check_result){
+
+                    var password = MD5($('#password').val());
+
+                    Socket.emit('Authorization', {'Email': $("#AuthForm").find('#email').val(), 'Password': password});
+                }
+            },
+            "Close": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+           
+    $('#LoginBtn').click(function(ev){
+        $("#AuthForm").dialog("open");
+    });
+    
     $("#LoginBtn").button();
     $("#RegisterBtn").button();
 });
