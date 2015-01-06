3fb987f81d93de6f6c06ec8f89eaed00b6c7658e
seqq86@gmail.com <p.nikolajevs@accenture.com>
2015-01-06 17:23:26 +0200

    Translation & design

--- web/bundles/deb/js/common.js
+++ web/bundles/deb/js/common.js
@@ -1,30 +1,5 @@
 $(function () {
-    
-    $("#AuthForm").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            "Reset": function() {
-                $("#AuthForm").find('#email').val("");
-                $('#password').val("");
-            },
-            "Submit": function() {
-
-                var check_result = UserModule.CheckAuthFields(password_length);
-
-                if(check_result){
-
-                    var password = MD5($('#password').val());
-
-                    Socket.emit('Authorization', {'Email': $("#AuthForm").find('#email').val(), 'Password': password});
-                }
-            },
-            "Close": function() {
-                $( this ).dialog( "close" );
-            }
-        }
-    });
-    
+            
     $("#RegForm").dialog({
         autoOpen: false,
         modal: true,
@@ -52,15 +27,10 @@ $(function () {
             }
         }
     });
-           
-    $('#LoginBtn').click(function(ev){
-        $("#AuthForm").dialog("open");
-    });
-    
+                   
     $('#RegisterBtn').click(function(ev){
         $("#RegForm").dialog("open");
     });
-    
-    $("#LoginBtn").button();
+        
     $("#RegisterBtn").button();
 });
