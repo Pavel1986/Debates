9336cdabd015aae0b2de1b5f6aa281cb18e654f1
seqq86@gmail.com <p.nikolajevs@accenture.com>
2015-01-06 17:43:53 +0200

    Registration fix [not finished]

--- web/bundles/deb/js/common.js
+++ web/bundles/deb/js/common.js
@@ -1,36 +1,5 @@
-$(function () {
-            
-    $("#RegForm").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            "Reset": function() {
-                $("#RegForm").find('#email').val("");
-                $('#password_first').val("");
-                $('#password_second').val("");
-                $('#name').val("");
-            },
-            "Create": function() {
-
-                var check_result = UserModule.CheckRegFields(name_length, password_length);
-
-                if(check_result){
-
-                    var password = MD5($('#password_first').val());
-
-                    Socket.emit('Registration', {'Email' : $("#RegForm").find('#email').val(), 'Password' : password, 'Name' : $('#name').val()});
-
-                }
-            },
-            "Close": function() {
-                $( this ).dialog( "close" );
-            }
-        }
-    });
-                   
-    $('#RegisterBtn').click(function(ev){
-        $("#RegForm").dialog("open");
-    });
-        
-    $("#RegisterBtn").button();
+$(function () {    
+    $("._submit").button();
+    $("._reset").button();
+    $("._close").button();
 });
