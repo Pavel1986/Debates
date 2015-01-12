1896b82ddf3d4b233b5f0fb4d790d17e249d4a95
seqq86@gmail.com <p.nikolajevs@accenture.com>
2015-01-12 15:26:58 +0200

    Saving chat messages to bd

--- web/bundles/deb/js/socket.js
+++ web/bundles/deb/js/socket.js
@@ -1,5 +1,16 @@
 $(function() {
-    socket.on('message', function (message) {
-        $("#MessagesList").append(message + "<br />");
-    });
+    
+    if(socket){    
+        socket.on('message', function (message) {
+            $("#MessagesList").append(new Date().toLocaleString() + ' ' +message + "<br />");
+        });
+        
+        socket.on('messages_list', function (data) {
+            $.each(data.messages, function(index, message) {
+                                
+                $("#MessagesList").append(new Date(message.date_created).toLocaleString() + ' ' + message.message + "<br />");
+                
+            });
+        });
+    }
 });
