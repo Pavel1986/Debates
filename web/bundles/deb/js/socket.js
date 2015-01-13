20482921ba93c99cbf9581b83a327cc08dbc0ac8
seqq86@gmail.com <p.nikolajevs@accenture.com>
2015-01-13 15:44:57 +0200

    Saving date in unix format

--- web/bundles/deb/js/socket.js
+++ web/bundles/deb/js/socket.js
@@ -2,13 +2,13 @@ $(function() {
     
     if(socket){    
         socket.on('message', function (message) {
-            $("#MessagesList").append(new Date().toLocaleString() + ' ' +message + "<br />");
+            $("#MessagesList").prepend(new Date().toLocaleString() + ' ' +message + "<br />");
         });
         
         socket.on('messages_list', function (data) {
             $.each(data.messages, function(index, message) {
                                 
-                $("#MessagesList").append(new Date(message.date_created).toLocaleString() + ' ' + message.message + "<br />");
+                $("#MessagesList").prepend(new Date(message.date_created).toLocaleString() + ' ' + message.message + "<br />");
                 
             });
         });
