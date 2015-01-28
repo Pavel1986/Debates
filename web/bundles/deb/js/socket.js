82996fd3ca7938bfd5bb0c8999566a64863b6542
seqq86@gmail.com <p.nikolajevs@accenture.com>
2015-01-28 18:59:51 +0200

    Add member to Topic ! Done !

--- web/bundles/deb/js/socket.js
+++ web/bundles/deb/js/socket.js
@@ -7,16 +7,14 @@ $(function() {
         
         socket.on('messages_list', function (data) {
             $.each(data.messages, function(index, message) {
-                                
                 $("#MessagesList").prepend(new Date(message.date_created).toLocaleString() + ' ' + message.message + "<br />");
-                
             });
         });
-        socket.on('TopicStartedAuthor', function () {
-            alert('TopicStarted. To author only.');
+        socket.on('TopicStartedAuthor', function (data) {
+            MainModule.ShowAlertWindow({ message : "TopicStarted. To author only.", title : "Topic started", force2redirect : true, redirect_link : "/detail/" + data.topic_id });
         });
-        socket.on('TopicStarted', function () {
-            alert('TopicStarted. In room.');
+        socket.on('TopicStarted', function (data) {
+            MainModule.ShowAlertWindow({ message : "TopicStarted. To room.", title : "Topic started", force2redirect : true, redirect_link : "/detail/" + data.topic_id });
         });
         
     }
